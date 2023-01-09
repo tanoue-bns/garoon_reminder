04c160edf2e5e900f3aa7b71fa1957eb2356dc35
tanoue-bns <tanoue@tenso.com>
2023-01-09 12:17:21 +0900

    feat: heroku → fly.ioへ移行

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@ const schedule = require('../lib/setSchedules')
 const cron = require('node-cron');
 
 module.exports = robot => {
-  // 毎日09:15に実行する
-  cron.schedule('15 9 * * *', () => {
+  // 毎日09:15に実行する（UTS基準なので 9-9=0）
+  cron.schedule('15 0 * * *', () => {
     schedule.setSchedules(robot);
   });
 }
